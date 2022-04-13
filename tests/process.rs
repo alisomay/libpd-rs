a730e69ea2ab1e2968f823d6b7a57a5b9e9fd235
Ali Somay <alisomay@runbox.com>
2022-04-14 01:35:36 +0200

    Add integration tests for process functions

--- tests/process.rs
+++ tests/process.rs
@@ -8,7 +8,10 @@ use libpd_rs::{
     block_size, close_patch,
     convenience::dsp_on,
     init, initialize_audio, open_patch,
-    process::{process_double, process_float, process_short},
+    process::{
+        process_double, process_float, process_raw, process_raw_double, process_raw_short,
+        process_short,
+    },
     receive::{on_bang, receive_messages_from_pd, start_listening_from, stop_listening_from},
     send::send_bang_to,
 };
@@ -79,7 +82,53 @@ fn all_process_functions() {
     assert_eq!(type_of(output_buffer[0]), "i16");
     assert_ne!(sum, 0);
 
-    dbg!(type_of(output_buffer[0]));
+    // Float Raw
+    let input_buffer = [0.0f32; 512];
+    let mut output_buffer = [0.0f32; 1024];
+
+    for _ in 0..(44100 / block_size() * output_channels) {
+        process_raw(&input_buffer, &mut output_buffer);
+    }
+
+    let sum = output_buffer.iter().fold(0_f32, |mut acc, element| {
+        acc += element;
+        acc
+    });
+
+    assert_eq!(type_of(output_buffer[0]), "f32");
+    assert_ne!(sum, 0.0);
+
+    // Double Raw
+    let input_buffer = [0.0f64; 512];
+    let mut output_buffer = [0.0f64; 1024];
+
+    for _ in 0..(44100 / block_size() * output_channels) {
+        process_raw_double(&input_buffer, &mut output_buffer);
+    }
+
+    let sum = output_buffer.iter().fold(0_f64, |mut acc, element| {
+        acc += element;
+        acc
+    });
+
+    assert_eq!(type_of(output_buffer[0]), "f64");
+    assert_ne!(sum, 0.0);
+
+    // Short Raw
+    let input_buffer = [0_i16; 512];
+    let mut output_buffer = [0_i16; 1024];
+
+    for _ in 0..(44100 / block_size() * output_channels) {
+        process_raw_short(&input_buffer, &mut output_buffer);
+    }
+
+    let sum = output_buffer.iter().fold(0_i32, |mut acc, element| {
+        acc += *element as i32;
+        acc
+    });
+
+    assert_eq!(type_of(output_buffer[0]), "i16");
+    assert_ne!(sum, 0);
 
     close_patch(patch_handle).unwrap();
 }
